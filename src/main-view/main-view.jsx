0458d2775b6bb2d073598be9bb096daf39c5625b
Trevor Higgins <higginst.th@gmail.com>
2024-07-18 11:03:32 -0700

    commit w/o useEffect

--- src/main-view/main-view.jsx
+++ src/main-view/main-view.jsx
@@ -8,22 +8,6 @@ const MainView = () => {
     const [movies, setMovies] = useState([]);
     const [selectedMovie, setSelectedMovie]= useState(null);
     
-    useEffect(() => {
-        fetch("https://movieurl-6be02303c42f.herokuapp.com/movies")
-          .then((response) => response.json())
-          .then((data) =>{
-            console.log(data);
-            const moviesFromApi = data.map((doc) => {
-              return {
-                id: doc._id,
-                title: doc.Title,
-                image: doc.ImagePath,
-                director: doc.Director.Name
-              };
-            });
-            setMovies(moviesFromApi);
-          });
-      }, []);
 
 if (selectedMovie) {
   return (<MovieView movieData = {selectedMovie} onBackClick={()=> {
