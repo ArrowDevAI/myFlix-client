5cddc664317506cc77fdfb60671d5fb63f87a614
Trevor Higgins <higginst.th@gmail.com>
2024-07-29 11:41:58 -0700

    updated

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -23,6 +23,7 @@ const SignupView = () => {
         method: "POST",
         body: JSON.stringify(data),
         headers: {"Content-Type": "application/json"}
+
     }).then ((response)=>{
         if (response.ok){
             alert ("Account Created Successfully");
@@ -34,45 +35,52 @@ const SignupView = () => {
 };
 
 return (
-    <form onSubmit = {handleSubmit}>
-        <label>
-            Username: <input 
-            type = "text"
-            value = {username}
-            onChange={(e)=> setUsername(e.target.value)} required
-            minLength = "5"
-            />
-        </label>
-
-        <label>
-            Password: <input
-            type= "password"
-            value = {password}
-            onChange ={(e)=> setPassword(e.target.value)}required
-            minLength = "5"
-            />
-            
-        </label>
-        <label>
-            Email: <input
-            type= "email"
-            value = {email}
-            onChange ={(e)=>setEmail(e.target.value)}required
-            minLength = "5"
-            />
-            
-        </label>
-        <label>
-             Birthday: <input
-             type= "date"
-             value = {birthday}
-             onChange ={(e)=>setBirthday(e.target.value)}required
-             minLength = "5"
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength="3"
         />
-        </label>
-        
-        <button type = "submit">Submit</button>
-    </form>
-)
-}
+      </Form.Group>
+
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group controlId="formEmail">
+        <Form.Label>Email:</Form.Label>
+        <Form.Control
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group controlId="formBirthday">
+        <Form.Label>Birthday:</Form.Label>
+        <Form.Control
+          type="date"
+          value={birthday}
+          onChange={(e) => setBirthday(e.target.value)}
+          required
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
 export {SignupView};
